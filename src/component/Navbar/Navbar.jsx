b2042a6af42c2a9bd0ee35b64dd62ad50f722715
naimurk <naimurk781@gmail.com>
2023-03-31 22:57:44 +0600

    bookmark blogs div add

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='border-b p-5'>
             <div className="navbar  bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">Programming Blogs</a>
+                    <a className="btn font-semibold  btn-ghost normal-case text-2xl">Programming Blogs</a>
                 </div>
                 <div className="flex-none gap-2">
                     <div className="form-control">
@@ -14,7 +14,7 @@ const Navbar = () => {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
                             </div>
                         </label>
                         <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
--- src/component/SideCart/SideCart.jsx
+++ src/component/SideCart/SideCart.jsx
@@ -5,6 +5,7 @@ const SideCart = (props) => {
     // console.log(props);
     // console.log(props);
     const cards = props.card
+    console.log(cards);
     const Time = props.time
     console.log(Time);
     
@@ -12,6 +13,9 @@ const SideCart = (props) => {
         <div className=' mt-11 bg-slate-100 p-3 w-4/12 border'>
             <div className='bg-white text-blue-600 text-xl font-semibold p-5'>
                 <h1>Spent Time on Read: {Time}</h1>
+            </div>
+            <div className='bg-white my-5 text-orange-600 text-xl font-semibold p-5'>
+                <h1> Bookmark Blogs: {cards.length}</h1>
             </div>
            {
             cards.map(card => <SingleSideCart card = {card}></SingleSideCart>)
